f6209c949b27158edd28676f551ccb35aa45bd03
ozzy92 <github.com@jpcrowley.com>
2018-02-10 16:05:46 +0000

    Add wss for heroku

--- connect4/static/connect4/connect4.js
+++ connect4/static/connect4/connect4.js
@@ -5,6 +5,7 @@ const board_columns = 7
 function connect_socket(url) {
     console.log('Connecting to socket ' + url);
 
-    var chat_socket = new ReconnectingWebSocket('ws://' + window.location.host + "/connect4/" + url);
+    var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
+    var chat_socket = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/connect4/" + url);
     return chat_socket
 }
