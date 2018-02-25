c5bc3c603d6eac2389e5a3bde61ade9b3580829a
ozzy92 <github.com@jpcrowley.com>
2018-02-25 23:49:47 +0000

    Async sorta works, need to make delays rather than sequential...

--- connect4/consumers.py
+++ connect4/consumers.py
@@ -9,7 +9,7 @@ import time
 import threading
 from asgiref.sync import async_to_sync, sync_to_async
 from django.utils import timezone
-from channels.consumer import SyncConsumer
+from channels.consumer import AsyncConsumer
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from channels.layers import get_channel_layer
 from .models import Game, ComputerPlayer
@@ -52,10 +52,27 @@ class GamesConsumer(AsyncJsonWebsocketConsumer):
     @classmethod
     def send_user_update(cls, player):
         cls._send_update(cls._CHANNEL_USER % player.get_short_name())
+
+    @classmethod
+    async def send_available_update_async(cls):
+        await cls._send_update_async(cls._CHANNEL_AVAILABLE)
+
+    @classmethod
+    async def send_running_update_async(cls):
+        await cls._send_update_async(cls._CHANNEL_RUNNING)
+
+    @classmethod
+    async def send_user_update_async(cls, player):
+        await cls._send_update_async(cls._CHANNEL_USER % player.get_short_name())
     
     @classmethod
     def _send_update(cls, channel):
-        ''' sends a group channel broadcast to these consumers '''
+        ''' sends a games list update to all clients '''
+        async_to_sync(cls._send_update_async)(channel)
+
+    @classmethod
+    async def _send_update_async(cls, channel):
+        ''' sends a games list update to all clients '''
         logger.info('Sending games update to channel %s' % channel)
         if channel == cls._CHANNEL_AVAILABLE:
             list = 'available_games'
@@ -64,7 +81,7 @@ class GamesConsumer(AsyncJsonWebsocketConsumer):
         else:
             list = 'user_games'
         channel_layer = get_channel_layer()
-        async_to_sync(channel_layer.group_send)(channel, {'type' : 'games.update', 'list' : list })
+        await channel_layer.group_send(channel, {'type' : 'games.update', 'list' : list })
 
     async def games_update(self, data):
         logger.info('Games update recieved, sending: %s; %s' % (self.channel_name, data))
@@ -104,13 +121,19 @@ class PlayConsumer(AsyncJsonWebsocketConsumer):
 
     @classmethod
     def send_play_update(cls, game):
-        ''' sends a group channel broadcast to these consumers '''
+        ''' sends a game play update to clients '''
+        async_to_sync(cls.send_play_update_async)(game)
+
+    @classmethod
+    async def send_play_update_async(cls, game):
+        ''' sends a game play update to clients '''
         channel_layer = get_channel_layer()
         channel = cls._CHANNEL_PLAY % game.id
-        logger.info('Sending sync player update to channel %s' % channel)
-        async_to_sync(channel_layer.group_send)(channel, { 'type' : 'play.update' })
+        logger.info('Sending async player update to channel %s' % channel)
+        await channel_layer.group_send(channel, { 'type' : 'play.update' })
 
     async def play_update(self, data):
+        ''' play update recieved, relay to clients '''
         logger.info('Games update received, sending: %s; %s' % (self.channel_name, data))
         await self.send_json(data)
 
@@ -121,20 +144,23 @@ class PlayConsumer(AsyncJsonWebsocketConsumer):
         await super().disconnect(close_code)
 
 
-class GameSeedConsumer(SyncConsumer):
+class GameSeedConsumer(AsyncConsumer):
     ''' This worker consumer creates games with computer players, and joins unstarted games '''
     
     SEEDED_GAMES = 5
-    JOIN_WAIT = 60
+    SEED_WAIT_MIN = 10
+    SEED_WAIT_MAX = SEED_WAIT_MIN * 5
+    JOIN_WAIT_MIN = 60
+    JOIN_WAIT_MAX = JOIN_WAIT_MIN * 5
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._added = False
-        self._join_thread = None
 
     @classmethod
     def register(cls):
         ''' tells seed consumer to start running '''
+        logger.info('Seed sending register')
         channel_layer = get_channel_layer()
         async_to_sync(channel_layer.send)(
             "game-seed",
@@ -143,53 +169,41 @@ class GameSeedConsumer(SyncConsumer):
             }
         )
 
-    def register_games(self, data):
+    async def register_games(self, data):
         ''' setup to receive games updates '''
-        logger.info('Seed register games')
+        logger.info('Seed register games received')
         if not self._added:
             self._added = True
             logger.info('Registering for group; %s' % self.channel_name)
-            async_to_sync(self.channel_layer.group_add)(GamesConsumer._CHANNEL_AVAILABLE, self.channel_name)
-        self._seed_games()
-        self._join_games_wait()
+            await self.channel_layer.group_add(GamesConsumer._CHANNEL_AVAILABLE, self.channel_name)
+        await self._seed_games()
+        await self._join_games()
 
-    def games_update(self, data):
+    async def games_update(self, data):
         ''' get a games update '''
         logger.info('Seed Games update; %s' % (self.channel_name))
-        self._seed_games()
-        self._join_games_wait()
+        await self._seed_games()
+        await self._join_games()
 
-    def _seed_games(self):
+    async def _seed_games(self):
         ''' adds games with computer players '''
-        # note: this will called repeatedly each time it creates a game
-        # because each game creates a new notifications
+        logger.info('Sleeping to seed games')
+        await asyncio.sleep(random.randint(self.SEED_WAIT_MIN, self.SEED_WAIT_MAX))
         logger.info('Seeding games')
         num_games = Game.objects.filter(status = Game.Status.AVAILABLE.value).count()
         if num_games < self.SEEDED_GAMES:
             logger.info('Adding new seeded game')
             player = random.choice(ComputerPlayer.objects.all())
             Game(player1 = player).save()
-            GamesConsumer.send_available_update()
-
-    def _join_games_wait(self):
-        ''' Schedules a callback for games to be joined '''
-        if not self._join_thread:
-            self._join_loop = asyncio.new_event_loop()
-            self._join_loop.set_debug(True)
-            def loop_thread():
-                asyncio.set_event_loop(self._join_loop)
-                self._join_loop.run_forever()
-            self._join_thread = threading.Thread(target = loop_thread)
-            self._join_thread.start()
-        asyncio.run_coroutine_threadsafe(self._join_games(), self._join_loop)
+            await GamesConsumer.send_available_update_async()
 
     async def _join_games(self):
         ''' Get any available games '''
         logger.info('Sleeping to check games')
-        await asyncio.sleep(self.JOIN_WAIT)
+        await asyncio.sleep(random.randint(self.SEED_WAIT_MIN, self.SEED_WAIT_MAX))
         logger.info('Checking for games to join')
         now = timezone.now()
-        aged = now - datetime.timedelta(seconds = self.JOIN_WAIT)
+        aged = now - datetime.timedelta(seconds = random.randint(self.JOIN_WAIT_MIN, self.JOIN_WAIT_MAX))
         games = Game.objects.filter(status = Game.Status.AVAILABLE.value, created_date__lt = aged)
         for game in games:
             logger.info('Joining game %s' % game)
@@ -199,11 +213,11 @@ class GameSeedConsumer(SyncConsumer):
             logger.info('Joining game %s; chose player %s' % (game, player))
             if game.join_up(player):
                 logger.info('Joined game %s; player %s' % (game, player))
-                GamesConsumer.send_available_update()
-                GamesConsumer.send_running_update()
-                PlayConsumer.send_play_update(game)
+                await GamesConsumer.send_available_update_async()
+                await GamesConsumer.send_running_update_async()
+                await PlayConsumer.send_play_update_async(game)
 
 
-class GamePlayerConsumer(SyncConsumer):
+class GamePlayerConsumer(AsyncConsumer):
     ''' This consumer plays a game as a computer player '''
 
